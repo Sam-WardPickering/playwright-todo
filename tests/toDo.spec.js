7f730ce65d9b9b493c27d34bdc817efe20db641c
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-02-17 17:47:42 +1300

    Change baseURL call

--- tests/toDo.spec.js
+++ tests/toDo.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('');
   
   await page.getByTestId('text-input').fill('Go for walk');
   await page.getByTestId('text-input').press('Enter');
