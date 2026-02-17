a76151880e46e942df34f58d38f6fc15caace043
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-02-17 18:35:03 +1300

    Refactor helper calls to use loop

--- tests/toDo.spec.js
+++ tests/toDo.spec.js
@@ -4,14 +4,20 @@ async function addTodo(page, text) {
     const input = page.getByTestId('text-input');
     await input.pressSequentially(text);
     await input.press('Enter');
-}
+};
+
+const todos = [
+    'Go for walk',
+    'Playwright Study',
+    'ISTQB Cert Study'
+]
 
 test('test adding todos @add', async ({ page }) => {
   await page.goto('');
-  
-  await addTodo(page, 'Go for walk');
-  await addTodo(page, 'Playwright Study');  
-  await addTodo(page, 'ISTQB Cert Study');
+
+  for(const todo of todos) {
+    await addTodo(page, todo);
+  };
 
   await expect(page.getByText('Go for walk')).toBeVisible();
   await expect(page.getByTestId('todo-list')).toContainText('Playwright Study');
