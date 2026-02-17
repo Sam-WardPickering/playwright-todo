b2f60095df6a83cfc518e6f94243b3c234816c33
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-02-17 18:30:22 +1300

    Create helper function

--- tests/toDo.spec.js
+++ tests/toDo.spec.js
@@ -1,19 +1,17 @@
 import { test, expect } from '@playwright/test';
 
+async function addTodo(page, text) {
+    const input = page.getByTestId('text-input');
+    await input.pressSequentially(text);
+    await input.press('Enter');
+}
+
 test('test adding todos @add', async ({ page }) => {
   await page.goto('');
-
-  const newTodo = page.getByTestId('text-input');
   
-  await newTodo.pressSequentially('Go for walk');
-  await newTodo.press('Enter');
- 
-  await newTodo.pressSequentially('Playwright Study');
-  await newTodo.press('Enter');
-  
-  await newTodo.pressSequentially('ISTQB Cert Study');
-  await newTodo.press('Enter');
-
+  await addTodo(page, 'Go for walk');
+  await addTodo(page, 'Playwright Study');  
+  await addTodo(page, 'ISTQB Cert Study');
 
   await expect(page.getByText('Go for walk')).toBeVisible();
   await expect(page.getByTestId('todo-list')).toContainText('Playwright Study');
