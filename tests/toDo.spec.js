4168ba3b97458bbdcc688ff6da887e2bdfbf131b
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-02-17 17:52:22 +1300

    Refactor test to reuse common variable

--- tests/toDo.spec.js
+++ tests/toDo.spec.js
@@ -1,16 +1,18 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('test adding todos', async ({ page }) => {
   await page.goto('');
+
+  const newTodo = page.getByTestId('text-input');
   
-  await page.getByTestId('text-input').fill('Go for walk');
-  await page.getByTestId('text-input').press('Enter');
+  await newTodo.fill('Go for walk');
+  await newTodo.press('Enter');
  
-  await page.getByTestId('text-input').fill('Playwright Study');
-  await page.getByTestId('text-input').press('Enter');
+  await newTodo.fill('Playwright Study');
+  await newTodo.press('Enter');
   
-  await page.getByTestId('text-input').fill('ISTQB Cert Study');
-  await page.getByTestId('text-input').press('Enter');
+  await newTodo.fill('ISTQB Cert Study');
+  await newTodo.press('Enter');
 
 
   await expect(page.getByText('Go for walk')).toBeVisible();
