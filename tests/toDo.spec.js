8b6118c7bbcb9dd872329b934ab17ddacfd17612
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-02-18 20:00:07 +1300

    Update playwright config to shorten slowmo time

--- tests/toDo.spec.js
+++ tests/toDo.spec.js
@@ -42,7 +42,7 @@ test('test adding todos @add', async ({ page }) => {
     // Navigate to active todos
     await page.getByRole('link', { name: 'Active' }).click();
 
-    const activeTodos = page.getByTestId('todo-list');
+    const activeTodos = await page.getByTestId('todo-list');
     // expect(toDoList).toBeEmpty();
     console.log(activeTodos);
 
